700bb58a99eea1ece1a8f15f952f813cbba95660
Derek Nissen <dereknissen23@gmail.com>
2025-02-10 17:33:05 -0600

    Testing a main function

---
+++ src/edu/mu/library/Main.java
@@ -0,0 +1,12 @@
+package edu.mu.library;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Book book1 = new Book();
+		book1.setAuthor("Derek Nissen");
+		System.out.println(book1.toString());
+	}
+
+}
