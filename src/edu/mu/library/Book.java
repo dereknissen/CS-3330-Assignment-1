e55a900ed5736e239297e2605e2839b50e002729
Derek Nissen <dereknissen23@gmail.com>
2025-02-10 10:58:32 -0600

    repo init

---
+++ src/edu/mu/library/Book.java
@@ -0,0 +1,14 @@
+package edu.mu.library;
+
+public class Book {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String title;
+		String author;
+		String ISBN;
+		double price;
+	}
+	
+
+}
