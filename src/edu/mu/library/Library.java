84c7ed9a8ee39bde9cc839db9927523448e7a4c1
CarterJC <carterjc730@gmail.com>
2025-02-11 15:03:22 -0600

    Add removeBook, searchByISBN, and displayBooks methods

--- src/edu/mu/library/Library.java
+++ src/edu/mu/library/Library.java
@@ -10,13 +10,48 @@ public class Library {
 	}
 	
 	public boolean addBook(Book book) {
-		if (count > 5) {
+		if (count > 4) {
 			return false;
 		}
 		else {
 			books[count] = book;
 			count++;
 			return true;
+		}
+	}
+	
+	public boolean removeBook(Book book) {
+		for (int i = 0; i < count; i++) {
+			if (books[i].equals(book)) {
+				for (int j = i; j < count - 1; j++) {
+					books[j] = books[j+1];
+				}
+				count--;
+				books[count] = null;
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public Book searchByISBN(String ISBN) {
+		for (int i = 0; i < count; i++) {
+			if (books[i].getISBN().equals(ISBN)) {
+				return books[i];
+			}
+		}
+		System.out.println("Test");
+		return null;
+	}
+	
+	public void displayBooks() {
+		if (count == 0) {
+			System.out.println("No books to print");
+		}
+		else {
+			for (int i = 0; i < count; i++) {
+				System.out.println(books[i].toString());
+			}
 		}
 	}
 }
